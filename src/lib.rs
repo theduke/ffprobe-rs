926ea0416ced6e8f4b0aa6dc032bd1b89b9e1934
Daniel Fidalgo <daniel@montra.com>
2024-09-17 17:49:41 -0500

    main - use i16
    main - add rotation to SideData
    
    Add rotation to side data and map it to Option<i8>

--- src/lib.rs
+++ src/lib.rs
@@ -225,6 +225,7 @@ pub struct Stream {
 #[allow(clippy::derive_partial_eq_without_eq)]
 pub struct SideData {
     pub side_data_type: String,
+    pub rotation: Option<i16>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
